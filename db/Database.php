b8bc632c514601847494da1130a94abf7e0c1cdf
Lazos <lazospap3@gmail.com>
2022-01-16 18:07:15 +0200

    ReadMe Test Update

--- db/Database.php
+++ db/Database.php
@@ -6,11 +6,11 @@ require_once "Db_upass.php";
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-if(gethostname()=='users.iee.ihu.gr') {
+if (gethostname() == 'users.iee.ihu.gr') {
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2015/it154586/mysql/run/mysql.sock');
 } else {
 		$pass=null;
-        $mysqli = new mysqli($host, $user, $pass, $db);
+        $mysqli = new mysqli($host, $user, '', $db);
 }
 
 if ($mysqli->connect_errno) {
