7d6981b0abc6fa6ca1db29dbdf8f08433e0cdb9f
Lazos <lazospap3@gmail.com>
2022-01-16 18:31:50 +0200

    Users Test

--- authentication/login.inc.php
+++ authentication/login.inc.php
@@ -4,8 +4,8 @@ if (isset($_POST["submit"])) {
     
     $name = $_POST["name"];
 
-    require_once '../db/Database.php';
-    require_once './functions_login.php';
+    require_once '/~it154586/ADISE21_KORPAP/db/Database.php';
+    require_once '/~it154586/ADISE21_KORPAP/authentication/functions_login.php';
 
     if(emptyInputLogin($name) !== false) {
         header("location: ./login.php?error=emptyinput");
