93264232aa39db32fcf67b58f4e21d60eb186e0c
Lazos <lazospap3@gmail.com>
2022-01-16 15:52:07 +0200

    Update Database.php

--- db/Database.php
+++ db/Database.php
@@ -6,8 +6,12 @@ require_once "Db_upass.php";
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-$mysqli = new mysqli($host, $user, $pass, $db);
-	
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2015/it154586/mysql/run/mysql.sock');
+} else {
+		$pass=null;
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
